3989a5bf4041a61b64e8ee158371d2060ef3083f
konabdigital378-prog <konabdigital378@gmail.com>
2026-06-27 23:56:33 +0100

    Fix: unregister old push-sw, clear all caches, no-cache headers, clean index.html

--- src/index.js
+++ src/index.js
@@ -4,6 +4,24 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    for (const reg of registrations) {
+      const url = reg.active?.scriptURL || reg.waiting?.scriptURL || reg.installing?.scriptURL || '';
+      if (url.includes('push-sw.js')) {
+        reg.unregister();
+      }
+    }
+  }).catch(() => {});
+  if ('caches' in window) {
+    caches.keys().then((names) => {
+      for (const name of names) {
+        caches.delete(name);
+      }
+    }).catch(() => {});
+  }
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -11,5 +29,4 @@ root.render(
   </React.StrictMode>
 );
 
-// Enregistrer le service worker pour PWA installable
 serviceWorkerRegistration.register();
